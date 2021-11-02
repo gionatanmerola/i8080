20ba9491ed4b5c221b73956cb56b8cb727ccad44
driverfury <driverfury@github.com>
2021-11-02 19:30:51 +0100

    types

---
+++ src/i8080.h
@@ -0,0 +1,11 @@
+#ifndef I8080_H
+#define I8080_H
+
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+typedef char  i8;
+typedef short i16;
+typedef int   i32;
+
+#endif
